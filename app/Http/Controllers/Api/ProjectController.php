0e4d510878ac8a1e65b34d35b188679203d190da
aSwanting <gab.damico@gmail.com>
2024-01-15 23:05:38 +0100

    add: eager beaver

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $results = Project::all();
+        $results = Project::with('type', 'technologies')->paginate(10);
         return response()->json([
             'results' => $results,
             'success' => true
